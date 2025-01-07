13ab9c580a09b28881c25652317d86c78af06b74
Kevin Jahns <kevin.jahns@protonmail.com>
2025-01-07 12:05:36 +0100

    build table embed

--- rollup.config.js
+++ rollup.config.js
@@ -1,75 +1,22 @@
 import nodeResolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 
-// If truthy, it expects all y-* dependencies in the upper directory.
-// This is only necessary if you want to test and make changes to several repositories.
-const localImports = process.env.LOCALIMPORTS
-
-const customModules = new Set([
-  'y-websocket',
-  'y-codemirror',
-  'y-ace',
-  'y-textarea',
-  'y-quill',
-  'y-dom',
-  'y-prosemirror'
-])
-/**
- * @type {Set<any>}
- */
-const customLibModules = new Set([
-  'lib0',
-  'y-protocols'
-])
-
-const yquillResolve = {
-  resolveId (importee) {
-    if (importee === 'y-quill') {
-      return `${process.cwd()}/src/y-quill.js`
-    }
-    if (importee === 'yjs') {
-      return `${process.cwd()}/node_modules/yjs/src/index.js`
-    }
-    return null
-  }
-}
-
-const debugResolve = {
-  resolveId (importee) {
-    if (localImports) {
-      if (importee === 'yjs/tests/testHelper.js') {
-        return `${process.cwd()}/../yjs/tests/testHelper.js`
-      }
-      if (importee === 'yjs') {
-        return `${process.cwd()}/../yjs/src/index.js`
-      }
-      if (customModules.has(importee.split('/')[0])) {
-        return `${process.cwd()}/../${importee}/src/${importee}.js`
-      }
-      if (customLibModules.has(importee.split('/')[0])) {
-        return `${process.cwd()}/../${importee}`
-      }
-    }
-    return null
-  }
-}
-
 export default [{
   input: './src/y-quill.js',
   output: [{
     name: 'yQuill',
     file: 'dist/y-quill.cjs',
     format: 'cjs',
-    sourcemap: true,
-    paths: path => {
-      if (/^lib0\//.test(path)) {
-        return `lib0/dist/${path.slice(5, -3)}.cjs`
-      }
-      if (/^y-protocols\//.test(path)) {
-        return `y-protocols/dist/${path.slice(12, -3)}.cjs`
-      }
-      return path
-    }
+    sourcemap: true
+  }],
+  external: id => /^lib0\//.test(id)
+}, {
+  input: './embeds/table-embed.js',
+  output: [{
+    name: 'tableEmbed',
+    file: 'dist/embeds/table-embed.cjs',
+    format: 'cjs',
+    sourcemap: true
   }],
   external: id => /^lib0\//.test(id)
 }, {
@@ -81,8 +28,6 @@ export default [{
     sourcemap: true
   }],
   plugins: [
-    yquillResolve,
-    debugResolve,
     nodeResolve({
       mainFields: ['module', 'browser', 'main']
     }),
@@ -97,8 +42,6 @@ export default [{
     sourcemap: true
   },
   plugins: [
-    yquillResolve,
-    debugResolve,
     nodeResolve({
       mainFields: ['module', 'browser', 'main']
     }),
